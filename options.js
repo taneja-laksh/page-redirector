2d2ee99e515412e5be1cc3c3994ace00610a8c78
Laksh <lakshtaneja02@gmail.com>
2025-07-18 13:49:45 +0530

    function to clean destination input, update ui, and update sync storage to add "destinationUrl"

--- options.js
+++ options.js
@@ -1,17 +1,22 @@
 async function updateui() {
     stored = await chrome.storage.sync.get("sitesToRedirect")
     stores = stored.sitesToRedirect || []
+    stored = await chrome.storage.sync.get("destinationUrl")
+    destinationstores = stored.destinationUrl || "127.0.0.1"
     joined = stores.join("\n")
     document.getElementById("pagetoredirect").value = joined
+    document.getElementById("destination").value = destinationstores
 }
 
 
 async function updatesites() {
     const sites = document.getElementById("pagetoredirect").value
     const sitesclean = sites.split("\n")
-
+    const desturl = document.getElementById("destination").value
+    destinationstores = cleanDestination(desturl)
+    console.log("url cleaned", desturl, " to ", destinationstores)
     chrome.storage.sync.set(
-        { sitesToRedirect: sitesclean }
+        { sitesToRedirect: sitesclean, destinationUrl: destinationstores }
     )
 
     chrome.runtime.sendMessage({ action: "refreshRedirectRules" })
@@ -22,6 +27,18 @@ async function updatesites() {
     setTimeout(() => {
         status.textContent = ''
     }, 750);
+}
+
+function cleanDestination(stored) {
+    if (stored) {
+        if (stored.slice(0, 7) == "http://" || stored.slice(0, 8) == "https://") {
+            return stored
+        } else {
+            return "https://" + stored
+        }
+    } else {
+        return "http://127.0.0.1"
+    }
 }
 
 updateui()
